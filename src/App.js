556030be6d68c5ae1b8f255abf935acd982f33a7
Shailendra2011 <shailendra.c@shyenatechyarns.in>
2026-05-02 07:28:49 +0530

    fixes done in layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { SubmitButton } from './submit';
 
 function App() {
   return (
-    <div>
+    <div className='flex h-screen w-screen flex-col overflow-hidden bg-background text-text-primary font-sans'>
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
